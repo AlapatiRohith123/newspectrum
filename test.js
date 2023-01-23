f1152301e6b3a3b794ae21f9ecad175f35ed3dcf
AlapatiRohith123 <123347987+AlapatiRohith123@users.noreply.github.com>
2023-01-23 10:19:26 +0530

    Add files via upload

---
+++ test.js
@@ -0,0 +1,15 @@
+/*var http = require('http');
+
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.end('Hello World!');
+}).listen(8080);*/
+var http = require('http');
+var fs = require('fs');
+http.createServer(function (req, res) {
+  fs.readFile('index.html', function(err, data) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write(data);
+    return res.end();
+  });
+}).listen(8080);
